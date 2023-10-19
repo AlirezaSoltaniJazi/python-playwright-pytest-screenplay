3b5bd840c882bf98451a3c9d1231a9a77edd41fe
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-19 20:19:01 +0330

    Separate config validator and put it to a function

--- features/config.py
+++ features/config.py
@@ -4,6 +4,7 @@ from os import path
 
 from pytest import fixture
 
+from utils.config_validator import validate_config
 from utils.logger_formatter import LOGGER
 from utils.project_directory_finder import get_project_directory
 
@@ -23,11 +24,5 @@ def config():
     with open(file_address, encoding='utf-8') as file_name:
         config_data = json.load(file_name)
         LOGGER.info('Config data', extra={'Config File Data': config_data})
-    config_data['browser'] = config_data['browser'].lower()
-    browser_name = config_data['browser']
-    supported_browsers = ['firefox', 'chrome', 'webkit']
-    if browser_name not in supported_browsers:
-        raise TypeError(
-            f'Browser {browser_name} is not supported, It must be one of: {supported_browsers}'
-        )
+    validate_config(config_data)
     return config_data

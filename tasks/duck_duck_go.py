332c0f2ee2a66fcc8d5b4623dfcc17f2342ae9b5
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-19 19:27:52 +0330

    Remove unnecessary function in task

--- tasks/duck_duck_go.py
+++ tasks/duck_duck_go.py
@@ -1,5 +1,3 @@
-from playwright.sync_api import Page
-
 from abilities.control_browser import ControlBrowser
 from questions.browser_info_retriever import BrowserInfoRetrieverQuestions
 
@@ -8,5 +6,4 @@ class DuckDuckGoTasks(ControlBrowser, BrowserInfoRetrieverQuestions):
 
     def perform_opening_duck_duck_go(self, url: str, page_title: str):
         self.navigate_to_url(url)
-        self.get_page_title()
         self.is_page_title_correct(page_title)
